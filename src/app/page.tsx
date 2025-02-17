0cd406984b4738d025d93bd7ce06a52a630ec906
Sergio Armando <keko.vgs@gmail.com>
2025-02-16 19:58:32 -0600

    adding styles to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,11 +19,16 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
-      {historyData.map((columnData: ColumnData) => {
+      {historyData.map((columnData: ColumnData, index: number) => {
         return (
-          <Column>
-            {columnData.map((element: DataElement) => {
-              return <div>{element.commits}</div>;
+          <Column key={`column-${index}`}>
+            {columnData.map((element: DataElement, index: number) => {
+              return (
+                <SingleCommit
+                  commits={element.commits}
+                  key={`commit-${index}`}
+                />
+              );
             })}
           </Column>
         );
@@ -34,4 +39,27 @@ export default function Home() {
 
 const Column = ({ children }: { children: ReactNode }) => {
   return <div className={styles.column}>{children}</div>;
+};
+
+const SingleCommit = ({ commits }: { commits: number }) => {
+  console.log(commits);
+  const backgroudColor = (count: number) => {
+    switch (count) {
+      case 1:
+      case 2:
+        return styles.l1;
+        break;
+      case 3:
+      case 4:
+        return styles.l2;
+        break;
+      case 5:
+      case 6:
+        return styles.l3;
+        break;
+      default:
+        return styles.l4;
+    }
+  };
+  return <div className={`${styles.item} ${backgroudColor(commits)}`}></div>;
 };
